b3abfb2992d1734548971c2ff34fc16368429da8
TomMahe <mahettom@gmail.com>
2023-04-13 23:59:45 +0200

    default section about until tech done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import Loader from '../Loader'
 import CanvasLoader from '../Loader'
 
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
@@ -25,8 +25,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.5, -1, 5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.5, -1, 5]}
         rotaition={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -35,7 +35,30 @@ const Computers = () => {
   )
 }
 
+
+
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)")
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
+
   return (
 
     <Canvas
@@ -51,7 +74,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
 
       </Suspense>
 
