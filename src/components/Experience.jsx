65c8bd697e7b369bc1b2683a29f65327b1bc8978
TomMahe <mahettom@gmail.com>
2023-05-04 15:18:13 +0200

    delete Vertical Timeline component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,3 @@
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
-import 'react-vertical-timeline-component/style.min.css'
 import { textVariant } from "../utils/motion"
 import { experiences } from "../constants"
 import { SectionWrapper } from "../hoc"
@@ -9,21 +7,7 @@ import { styles } from "../styles"
 
 const ExperienceCard = ({ experience }) => (
 
-  <VerticalTimelineElement
-    contentStyle={{ background: 'white', color: 'black', font: 'font-ledger', border: '7px solid #232631' }}
-    contentArrowStyle={{ borderRight: '7px solid #232631' }}
-    date={experience.date}
-    iconStyle={{ background: 'black' }}
-    icon={
-      <div className="flex justify-center items-center w-full h-full">
-        <img
-          src={experience.icon}
-          alt={experience.company_name}
-          className='w-[60%] h-[60%] object-contain'
-        />
-      </div>
-    }
-  >
+  <>
     <div>
       <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
       <p className="text-secondary text-[16px] font-semibold" style={{ margin: 0 }}>{experience.company_name}</p>
@@ -39,7 +23,7 @@ const ExperienceCard = ({ experience }) => (
         </li>
       ))}
     </ul>
-  </VerticalTimelineElement>
+  </>
 )
 
 
@@ -53,11 +37,9 @@ const Experience = () => {
         <h2 className={styles.sectionHeadText}>Work Experience</h2>
       </motion.div>
       <div className='mt-20 flex flex-col'>
-        <VerticalTimeline>
-          {experiences.map((experience, index) =>
-            <ExperienceCard key={index} experience={experience} />
-          )}
-        </VerticalTimeline>
+        {experiences.map((experience, index) =>
+          <ExperienceCard key={index} experience={experience} />
+        )}
       </div>
     </>
   )
