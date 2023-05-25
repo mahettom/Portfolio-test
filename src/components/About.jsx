0747df86fca18b503074ea20d187fdd573fc13db
TomMahe <mahettom@gmail.com>
2023-05-26 01:19:34 +0200

    section created: hoverflow needed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,32 +29,30 @@ const About = () => {
 
   return (
 
-    <>
-      <div className='bg-gray-100 flex items-center gap-10 h-80 rounded-2xl mt-36 xs:mt-30'>
 
-        <div className='flex flex-col w-7/12 items-center p-36'>
-          <h2 className={`${styles.sectionHeadText} text-primary font-bold ml-[-100px]`}> Vision </h2>
+    <div className='  overflow-y-scroll flex items-center justify-end gap-10 rounded-2xl h-80 my-36 xs:my-30 bg-gray-100'>
 
-          <p className='mt-10 text-primary font-ledger text-[16px] leading-8'>
-            J'ai été formé pour devenir un developpeur fullstack à Ironhack.
-            <br /><br />
-            Et je voudrais maintenant:
-          </p>
-        </div>
+      <div className=' flex flex-col w-7/12 items-center pl-4'>
+        <h2 className={`${styles.sectionHeadText} text-primary font-bold ml-[-100px]`}> Vision </h2>
 
-
-        <div className='flex flex-col items-center gap-7 h-40 w-40'>
-
-          {objectifs.map((objectif, index) => (
-            <ServiceCard key={objectif.title} index={index} {...objectif} />
-          ))}
-
-        </div>
-
+        <p className='mt-10 text-primary font-ledger text-[16px] leading-8'>
+          J'ai été formé pour devenir un developpeur fullstack à Ironhack.
+          <br /><br />
+          Et je voudrais maintenant:
+        </p>
       </div>
 
 
-    </>
+      <div className=' flex flex-col items-center gap-7 h-full w-full'>
+
+        {objectifs.map((objectif, index) => (
+          <ServiceCard key={objectif.title} index={index} {...objectif} />
+        ))}
+
+      </div>
+
+    </div>
+
   )
 }
 
