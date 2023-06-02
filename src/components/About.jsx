bc909c2e401d78d5d4bc6d66dc3f0afc57b2c90d
TomMahe <mahettom@gmail.com>
2023-06-02 22:16:02 +0200

    cleanning

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,30 +28,30 @@ const About = () => {
 
 
   return (
-    <>
-      <div className='  flex items-center h-screen w-screen mt-[-70px] mb-[-70px]'>
-        <div className=' relative flex items-center md:justify-start xs:justify-center w-6/12 md:w-10/12 h-96 rounded-2xl bg-gray-100 text-center'>
 
-          <div className=' w-9/12 '>
-            <h2 className={`${styles.sectionHeadText} text-primary font-bold text-center`}> Vision </h2>
+    <section className='flex items-center h-screen w-screen'>
 
-            <p className='mt-10 text-primary font-ledger text-[16px] xs:text-[14px] leading-8'>
-              J'ai été formé pour devenir un developpeur fullstack à Ironhack.
-              <br /><br />
-              Et je voudrais maintenant:
-            </p>
-          </div>
+      <div className='relative flex flex-col justify-center items-center text-center w-10/12 h-96 rounded-2xl bg-gray-100 '>
+        <div className=' w-9/12 '>
+          <h2 className={`${styles.sectionHeadText} text-primary font-bold text-center`}> Vision </h2>
+
+          <p className='mt-10 text-primary font-ledger text-[16px] xs:text-[14px] leading-8'>
+            J'ai été formé pour devenir un developpeur fullstack à Ironhack.
+            <br />
+            Et je voudrais maintenant:
+          </p>
+
         </div>
       </div>
 
       <div className='absolute overflow-visible top-48 left-80 flex flex-col gap-4 h-96  md:flex-row md:w-full md:top-52 md:left-10 md:overflow-x-auto md:px-56'>
-
         {objectifs.map((objectif, index) => (
           <ServiceCard key={objectif.title} index={index} {...objectif} />
         ))}
-
       </div>
-    </>
+
+    </section>
+
   )
 }
 
