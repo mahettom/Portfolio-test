37f3a26a755c024879c14e97148b7b414d3fc989
TomMahe <mahettom@gmail.com>
2023-06-05 16:43:29 +0200

    fix padding and font-size

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,7 +19,7 @@ const ProjectCard = (project) => {
     <>
       <div className='relative w-full'>
         <div className='absolute flex justify-end xs:right-2 sm:right-4 md:right-5'>
-          <div onClick={() => window.open(project.source_code_link, '_blank')} className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+          <div onClick={() => window.open(project.source_code_link, '_blank')} className='bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
             <img
               src={github}
               alt='github'
@@ -34,14 +34,14 @@ const ProjectCard = (project) => {
         ?
 
         <div>
-          <p className='text-primary font-ledger text-[16px] text-center'>{project.description}</p>
+          <p className={`${styles.expWorkList} text-primary`}>{project.description}</p>
           <img src={project.image} alt={project.name} className='w-1/3 h-full object-cover rounded-2xl m-auto' />
         </div>
 
         :
 
         <ul className='list-disc px-6 text-primary font-ledger text-[16px]'>
-          {infoToDisplay.map((info, index) => <li key={index} className='text-primary font-ledger text-[16px]'>{info}</li>)}
+          {infoToDisplay.map((info, index) => <li key={index} className={`${styles.expWorkList} text-[0.65rem] text-primary`}>{info}</li>)}
         </ul >
 
       }
@@ -71,25 +71,24 @@ const Works = () => {
 
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-primary`}>My work</p>
+      <motion.div variants={textVariant()} className='m-4'>
         <h2 className={`${styles.sectionHeadText} text-primary`}>Project.</h2>
       </motion.div>
 
 
-      <div className='flex flex-row py-36'>
+      <div className='flex flex-col gap-4'>
 
-        <div className='flex flex-col justify-evenly items-center'>
+        <div className='flex flex-row justify-evenly items-center'>
           {projects.map((project, index) => (
             <button onClick={() => setActualProject(project)} className='font-ledger text-primary w-56 p-5 border-4 rounded-md shadow-inner' key={`project-${index}`}>{project.name}</button>
           ))}
         </div>
 
-        <div className='flex flex-col'>
+        <div className='flex flex-col h-80'>
           <ProjectCard {...actualProject} info={infoToDisplay} />
         </div>
 
-        <div className='flex flex-col justify-evenly'>
+        <div className='flex flex-row justify-evenly'>
           <button onClick={() => setInfoToDisplay(requirement)} className='font-ledger text-primary p-5 border-4 rounded-md shadow-inner'>Requirement</button>
           <button onClick={() => setInfoToDisplay(challenge)} className='font-ledger text-primary p-5 border-4 rounded-md shadow-inner'>Challenge</button>
           <button onClick={() => setInfoToDisplay(improvement)} className='font-ledger text-primary p-5 border-4 rounded-md shadow-inner'>Improvement</button>
